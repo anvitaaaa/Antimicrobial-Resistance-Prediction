719ec1daa0b651c6d34a9e54c9d6fa03c0be4f02
Anvita <anvita192005@gmail.com>
2025-10-18 20:20:18 +0530

    test

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv(
-    "data/raw/isolates.csv",
-    low_memory=False,
-    on_bad_lines='skip'   # skip malformed rows
-)
-print(df.columns)
-print(df['AST phenotypes'].head())
